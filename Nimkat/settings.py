2bf6b27f21f0c51e885b6db2f0e8ea76d31ec5d1
mohammad maso <mohammadmasoudie@gmail.com>
2021-07-13 02:07:04 +0430

    -

--- Nimkat/settings.py
+++ Nimkat/settings.py
@@ -86,11 +86,8 @@ TEMPLATES = [
     },
 ]
 
-CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_ALLOW_ALL = True
 
-CORS_ORIGIN_WHITELIST = (
-    'http://localhost:3000/',
-)
 
 GRAPHENE = {
     "SCHEMA": "Nimkat.schema.schema",
