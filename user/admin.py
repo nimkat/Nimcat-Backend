c8284a550418afb679f5805399d56f65837e2c27
mohammad maso <mohammadmasoudie@gmail.com>
2021-08-17 14:04:00 +0430

    add usr admin view

--- user/admin.py
+++ user/admin.py
@@ -1,8 +1,10 @@
 from user.models import User
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
 from .models import User, ProfileModel, SMSVerificationCodes, BoughtCoursesModel
 
-admin.site.register(User)
+admin.site.register(User,UserAdmin)
 admin.site.register(ProfileModel)
 admin.site.register(SMSVerificationCodes)
 admin.site.register(BoughtCoursesModel)
